08da5549cc66fba68c0077d42c7266a37c57c23c
Storm B. Heg <storm@stormbase.digital>
2024-02-20 16:12:34 +0100

    Fix templates for Wagtail 6.0
    The {% field %} templatetag was renamed {% formattedfield %} in https://github.com/wagtail/wagtail/pull/11271

--- src/wagtail_2fa/views.py
+++ src/wagtail_2fa/views.py
@@ -31,10 +31,12 @@ from wagtail_2fa.mixins import OtpRequiredMixin
 
 class LoginView(RedirectURLMixin, FormView):
 
-    if WAGTAIL_VERSION >= (5, 0):
-        template_name = "wagtail_2fa/otp_form.html"
+    if WAGTAIL_VERSION >= (6, 0):
+        template_name = "wagtail_2fa/compat/otp_form_wagtail60.html"
+    elif WAGTAIL_VERSION >= (5, 0):
+        template_name = "wagtail_2fa/compat/otp_form_wagtail5x.html"
     else:
-        template_name = "wagtail_2fa/legacy/otp_form.html"
+        template_name = "wagtail_2fa/compat/otp_form_wagtail4x.html"
 
     form_class = forms.TokenForm
     redirect_field_name = REDIRECT_FIELD_NAME
